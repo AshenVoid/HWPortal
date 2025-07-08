271a14e542bfc1be88cf0cb82bdf3fd3054dc3f3
AshenVoid <richardegydy@gmail.com>
2025-07-08 18:30:14 +0200

    templates: base, home, reviews, components concepts

--- HWPortal/viewer/urls.py
+++ HWPortal/viewer/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    #path('home/', views.home, name='home'),
+    path('components/', views.components, name='components'),
+    path('reviews/', views.reviews, name='reviews'),
 ]
--- HWPortal/viewer/views.py
+++ HWPortal/viewer/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'viewer/home.html')
+
+def components(request):
+    return render(request, 'viewer/components.html')
+
+def reviews(request):
+    return render(request, 'viewer/reviews.html')
