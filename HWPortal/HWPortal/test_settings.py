61953667431532af27a171f3c9fdb19a71ab825e
AshenVoid <richardegydy@gmail.com>
2025-07-27 12:27:10 +0200

    tests: modely, vytváření, integrace, validita

---
+++ HWPortal/HWPortal/test_settings.py
@@ -0,0 +1,51 @@
+# test_settings.py
+from .settings import *
+
+# Pro testy použití SQLite v paměti
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# Zrychli testy
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# Vypni migrace cache pro testy
+MIGRATION_MODULES = {
+    'viewer': None,
+}
+
+# Debug pro testy
+DEBUG = True
+
+# Zjednodušené logování pro testy
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
+# Vypni některé middleware pro rychlejší testy
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+# Pro Selenium testy
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
